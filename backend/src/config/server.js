2e94060a47399e0d2e9f449a44746033701d0658
Cleiton <cleitondantasdeoliveira@hotmail.com>
2018-03-18 23:07:08 -0300

    Webpack config na pasta certa. Criação do primeiro componente

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json())
+server.use(bodyParser.json());
 
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`)
